ae4c0cf0e7c10f17148e32060333c3c836639e14
Baker Bokorney <bbokorney@gmail.com>
2021-02-15 08:54:04 -0700

    Fix current month where clause

--- pkg/sqlutil/sqlutil.go
+++ pkg/sqlutil/sqlutil.go
@@ -10,8 +10,8 @@ import (
 func CurrentMonthWhereClause(db *gorm.DB) *gorm.DB {
 	today := time.Now()
 	year, month := today.Year(), today.Month()
-	start := fmt.Sprintf("%d-%d-01 00:00", year, month)
-	end := fmt.Sprintf("%d-%d-01 00:00", year, (month+1)%12)
+	start := fmt.Sprintf("%d-%02d-01 00:00", year, month)
+	end := fmt.Sprintf("%d-%02d-01 00:00", year, (month+1)%12)
 	return db.Where("date BETWEEN ? AND ?", start, end)
 }
 
